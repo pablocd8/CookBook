b8cdc2cd30310f688f40872ac6b9f8ee3418de80
Pablo <pablocerdadonat8@gmail>
2025-01-16 12:02:02 +0100

    ImplementacionHome

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,100 +1,107 @@
 import Image from "next/image";
+import "./globals.css";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <div className="container">
+      <header className="header">
+        <h1 className="title">
+          Bienvenido a <span className="highlight">CookBook Online</span>
+        </h1>
+        <p className="subtitle">
+          Descubre, comparte y gestiona recetas de cocina en un solo lugar.
+        </p>
+      </header>
+      <main className="main">
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+          src="/images/super-chef-fork-spatula-chef-hero-mascot-vector-illustration-mascot-logo-image-super-chef-fork-spatula-chef-hero-197636829.webp"
+          alt="Hero de CookBook Online"
+          width={400}
+          height={200}
+          className="heroImage"
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+   
+        <div className="containerBusquedas">
+          <h2 className="tituloBusquedas">Explora nuevas recetas</h2>
+          <p className="sectionSubtitulos">
+            Encuentra platos principales, postres, recetas veganas y mucho más.
+          </p>
+          <div className="buttonsHome">
+            <a href="" className="buttonExplorarRecetas">
+              Explorar Recetas
+            </a>
+            <a href="" className="buttonLogin">
+              Registrarse
+            </a>
+          </div>
         </div>
+
+    
+        <section className="features">
+          <div className="feature">
+          <Image
+            src="/images/simbolo-de-la-interfaz-de-busqueda.png" 
+            alt="Recetas favoritas"
+            width={40}
+            height={40}
+          />
+            <div>
+              <h3 className="featureTitulo">Búsqueda Avanzada</h3>
+              <p className="featureDescripcion">
+                Filtra recetas por ingredientes, tiempo de preparación o
+                dificultad.
+              </p>
+            </div>
+          </div>
+          <div className="feature">
+            <Image
+              src="/images/favorito.png"
+              alt="Recetas favoritas"
+              width={40}
+              height={40}
+            />
+            <div>
+              <h3 className="featureTitulo">Recetas Favoritas</h3>
+              <p className="featureDescripcion">
+                Guarda tus recetas favoritas y accede a ellas fácilmente.
+              </p>
+            </div>
+          </div>
+          <div className="feature">
+            <Image
+              src="/images/compartir.png"
+              alt="Compartir recetas"
+              width={40}
+              height={40}
+            />
+            <div>
+              <h3 className="featureTitulo">Comparte tus Recetas</h3>
+              <p className="featureDescripcion">
+                Sube tus propias recetas y comparte tu creatividad culinaria.
+              </p>
+            </div>
+          </div>
+          <div className="feature">
+            <Image
+              src="/images/comentario.png"
+              alt="Comunidad de cocina"
+              width={40}
+              height={40}
+            />
+            <div>
+              <h3 className="featureTitulo">Comunidad Activa</h3>
+              <p className="featureDescripcion">
+                Interactúa con otros usuarios dejando comentarios y valoraciones.
+              </p>
+            </div>
+          </div>
+        </section>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+
+    
+      <footer className="footer">
+        © 2025 CookBook Online. Todos los derechos reservados.
       </footer>
     </div>
   );
